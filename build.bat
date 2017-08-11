122b2bbca0988fd65136dde7bc725d29df96503a
Guillaume Dumont <dumont.guillaume@gmail.com>
2017-08-10 22:28:51 -0400

    Updated build image and script

--- build.bat
+++ build.bat
@@ -3,28 +3,33 @@
 set ROOT_DIR=C:\Projects
 set VCPKG_DIR=C:\Projects\vcpkg
 
-if EXIST "%VCPKG_DIR%" (goto build_project)
+if EXIST "%VCPKG_DIR%\vcpkg.exe" ( goto install_dependencies )
 
 REM install vcpkg and dependencies
 if NOT EXIST "%ROOT_DIR%" (mkdir "%ROOT_DIR%")
 pushd "%ROOT_DIR%"
-git clone https://github.com/willyd/vcpkg.git
-pushd vcpkg
+
+git clone https://github.com/willyd/vcpkg.git .
 git checkout opencv-static
 
+pushd  "%VCPKG_DIR%"
+
+REM vcpkg prompts to download those so we download them manually
 powershell -NoProfile -Exec ByPass -Command ".\scripts\fetchDependency.ps1 -Dependency cmake -downloadPromptOverride '1'"
 powershell -NoProfile -Exec ByPass -Command ".\scripts\fetchDependency.ps1 -Dependency nuget -downloadPromptOverride '1'"
 powershell -NoProfile -Exec ByPass -Command ".\scripts\fetchDependency.ps1 -Dependency git -downloadPromptOverride '1'"
 
-call bootstrap-vcpkg.bat
-.\vcpkg.exe install glog gflags eigen3 protobuf lmdb --triplet x64-windows-static
+REM build vcpkg if it does not exist
+if NOT EXIST "%VCPKG_DIR%\vcpkg.exe" ( call bootstrap-vcpkg.bat )
+
 popd
 popd
 
-:build_project
-REM list installed packages
-pushd "%VCPKG_DIR%"
-vcpkg list
+:install_dependencies
+pushd  "%VCPKG_DIR%"
+REM install some of the dependencies
+.\vcpkg.exe install glog gflags eigen3 protobuf lmdb --triplet x64-windows-static
+.\vcpkg list
 popd
 
 REM build project
