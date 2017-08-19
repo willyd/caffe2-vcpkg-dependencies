8196984d16a5a51b4912c1f8dcc86b262a012197
Guillaume Dumont <dumont.guillaume@gmail.com>
2017-08-18 20:22:25 -0400

    Trying to build caffe dependencies

--- scripts/appveyor/install_dependencies.bat
+++ scripts/appveyor/install_dependencies.bat
@@ -23,8 +23,12 @@ powershell -NoProfile -Exec ByPass -Command ".\scripts\fetchDependency.ps1 -Depe
 REM build vcpkg if it does not exist
 call bootstrap-vcpkg.bat
 
+.\vcpkg.exe install msmpi
+set ERRORLEVEL=0
+powershell -NoProfile -Exec ByPass -Command "$setup = gci downloads\MSMpiSetup*.exe; . $setup.FullName"
+
 REM install some of the dependencies
-.\vcpkg.exe install gflags glog eigen3 protobuf lmdb --triplet x64-windows-static
+.\vcpkg.exe install gflags glog boost hdf5 openblas protobuf lmdb --triplet x64-windows-static
 
 REM remove unncessary stuff
 rmdir /S /Q .git
